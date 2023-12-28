e04ea348f877814f6866af64a7eea25efe8240d3
Shawn Kearney <shawnpk29@me.com>
2023-12-28 09:30:28 -0800

    Adjust specs table

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require 'factory_bot_rails'
+# require "factory_bot_rails"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
